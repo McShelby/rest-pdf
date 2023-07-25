59c3908df115adfe0d208c9d7a17c18f18254840
Sören Weber <mail@soeren-weber.de>
2023-07-26 00:30:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# REST PDF
+
+A REST endpoint to convert HTML URLs to PDF using Node.js
+
+A quick and dirty hack with no error handling. Use at your own risk.
+
+## Requirements
+
+- Node.js (tested with v20.5.0)
+
+## Install
+
+````
+npm ci
+````
+
+## Run
+
+````
+npm start
+````
+
+## Test
+
+In your browser open
+
+````
+http://localhost:3000/pdf?target=https://example.com
+````
+
+## URL Parameter
+
+| Name     | Default | Description |
+| -------- | ------- | ----------- |
+| `target` |         | URL to convert, eg. `https://example.com` |
+| `format` | `A4`    | [Puppeteer paperformat](https://pptr.dev/api/puppeteer.paperformat) |
---
+++ index.js
@@ -0,0 +1,55 @@
+import puppeteer from 'puppeteer';
+import express from 'express';
+
+const app = express();
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight - window.innerHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
+app.get("/pdf", async (req, res) => {
+    const url = req.query.target;
+    const format = req.query.format || 'A4';
+
+	const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+	await page.goto( url, {
+        waitUntil: 'networkidle2'
+    });
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
+    await autoScroll(page);
+
+	const pdf = await page.pdf({
+        format: format,
+        printBackground: true,
+        scale: 1,
+    });
+
+	await browser.close();
+
+	res.contentType("application/pdf");
+    res.send(pdf);
+});
+
+app.listen(3000, () => {
+    console.log("Server started");
+});
